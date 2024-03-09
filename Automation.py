b0c470cbcc65b3a60761e0337cf38eae9bc22fbb
MWPDT <144591377+qiuzhiche@users.noreply.github.com>
2024-03-09 19:43:31 +0800

    V0.2.0发布！！！

--- Automation.py
+++ Automation.py
@@ -1,36 +1,40 @@
 import os
-
-current_directory = os.getcwd()
-config_folder_path = os.path.join(current_directory, "Config")
-
-if os.path.exists(config_folder_path) and os.path.isdir(config_folder_path):
-    print("\033[32m[INFO]:Config文件夹存在,程序运行\033[0m")
-    print("\033[32m自动化图像识别\033[0m")
-    print("\033[32m作者：MWPDT\033[0m")
-    print("\033[32mV1.0\033[0m")
-else:
-    print("\033[32m[INFO]:正在创建名为'Config'文件夹\033[0m")
-    path = "Config/picture"
-    os.makedirs(path)
-    print("\033[32m[INFO]:'Config'文件夹创建成功\033[0m")
-    print("\033[32m[INFO]:正在Config中创建名为'picture'文件夹\033[0m")
-    print("\033[32m[INFO]:'picture'文件夹创建成功\033[0m")
-    # 在程序运行目录下创建名为"Config"的文件夹，在名为"Config"的文件夹再创建一个名为"picture"
-
-import os
 import cv2
 import numpy as np
 import pyautogui
 import keyboard
 import tkinter as tk
 from tkinter import filedialog
-
-# 获取当前程序运行目录
-current_dir = os.path.dirname(os.path.abspath(__file__))
-picture_folder = os.path.join(current_dir, 'Config', 'picture')
+import pytesseract
+from PIL import Image
 
 
-def browse_images():
+def create_folders():
+    current_directory = os.getcwd()
+    config_folder_path = os.path.join(current_directory, "Config")
+
+    if not os.path.exists(config_folder_path):
+        print("\033[32m[INFO]: 正在创建名为 'Config' 文件夹\033[0m")
+        os.makedirs(config_folder_path)
+        print("\033[32m[INFO]: 'Config' 文件夹创建成功\033[0m")
+
+        picture_folder_path = os.path.join(config_folder_path, "picture")
+        print("\033[32m[INFO]: 正在 Config 中创建名为 'picture' 文件夹\033[0m")
+        os.makedirs(picture_folder_path)
+        print("\033[32m[INFO]: 'picture' 文件夹创建成功\033[0m")
+        input("\033[32m[INFO]: 按回车键退出......\033[0m")
+    else:
+        print("\033[32m[INFO]: Config 文件夹存在，程序运行\033[0m")
+        text_recognition_folder_path = os.path.join(config_folder_path, "Text Recognition")
+        if not os.path.exists(text_recognition_folder_path):
+            print("\033[32m[INFO]: 正在 'Config' 中创建名为 'Text Recognition' 文件夹\033[0m")
+            os.makedirs(text_recognition_folder_path)
+            print("\033[32m[INFO]: 'Text Recognition' 文件夹创建成功\033[0m")
+        else:
+            print("\033[32m[INFO]: 'Text Recognition' 文件夹已存在\033[0m")
+
+
+def browse_images(picture_folder=None):
     root = tk.Tk()
     root.withdraw()
 
@@ -43,51 +47,102 @@ def browse_images():
         if target_image is not None:
             target_images.append(target_image)
         else:
-            print(
-                f"\033[31m[error]: 无法读取图像 {image_file},请将文件名改成target_image如果有多个图像请按顺序因此递增(例如：target_image1 "
-                f"target_image2 target_image3...)\033[0m")
-            input("\033[32m[INFO]：按回车键退出\033[0m")
+            print(f"\033[31m[error]: 无法读取图像 {image_file}，请检查文件名是否正确\033[0m")
     return target_images
 
 
-def main():
+def perform_image_recognition():
+    create_folders()
+
     target_images = browse_images()
     if not target_images:
         print("\033[31m[error]: 未选择要识别的图像\033[0m")
         return
-    print("\033[32m[INFO]：为了防止鼠标失控,必要时按F8退出\033[0m")
-    running = True
-    while running:
-        # 读取屏幕截图并转换为numpy数组
+
+    print("\033[32m[INFO]: 为了防止鼠标失控, 必要时按F8退出\033[0m")
+
+    while True:
         screenshot = pyautogui.screenshot()
         screenshot = np.array(screenshot)
         screenshot = cv2.cvtColor(screenshot, cv2.COLOR_RGB2BGR)
 
         for target_image in target_images:
-            # 在屏幕截图中查找目标图像
             result = cv2.matchTemplate(screenshot, target_image, cv2.TM_CCOEFF_NORMED)
-            min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+            _, max_val, _, max_loc = cv2.minMaxLoc(result)
 
-            if max_val > 0.8:  # 设置阈值，确保识别准确性
-                print("\033[32m[INFO]:识别到图像,移动点击\033[0m")
-                # 输出目标图像位置
+            if max_val > 0.8:
+                print("\033[32m[INFO]: 识别到图像，移动并点击\033[0m")
                 target_width, target_height = target_image.shape[1], target_image.shape[0]
                 top_left = max_loc
                 bottom_right = (top_left[0] + target_width, top_left[1] + target_height)
-                print(f'\033[32m[INFO]:图像屏幕空间坐标: {top_left} - {bottom_right}\033[0m')
+                print(f'\033[32m[INFO]: 图像屏幕空间坐标: {top_left} - {bottom_right}\033[0m')
 
-                # 移动鼠标并点击目标图像中心
                 target_center = (top_left[0] + target_width // 2, top_left[1] + target_height // 2)
                 pyautogui.moveTo(target_center)
                 pyautogui.click()
             else:
-                print("\033[32m[INFO]:未识别到图像\033[0m")
+                print("\033[32m[INFO]: 未识别到图像\033[0m")
 
-        # 检测是否按下F8键，如果是则结束循环
         if keyboard.is_pressed('f8'):
-            running = False
-            input("\033[32m[INFO]：按回车键退出\033[0m")
+            break
+
+    input("\033[32m[INFO]: 按回车键退出......\033[0m")
+
+
+def perform_text_recognition():
+    create_folders()
+
+    text_recognition_dir = os.path.join(os.getcwd(), "Config", "Text Recognition")
+    file_path = os.path.join(text_recognition_dir, "recognition_result.txt")
+
+    root = tk.Tk()
+    root.withdraw()
+
+    image_files = filedialog.askopenfilenames(title="选择图片文件",
+                                              filetypes=(("图片文件", "*.jpg *.png"), ("所有文件", "*.*")))
+
+    langs = ['eng', 'chi_sim']  # 英语和中文
+    with open(file_path, "a", encoding='utf-8') as file:
+        for image_file in image_files:
+            img = Image.open(image_file)
+            for lang in langs:
+                data = pytesseract.image_to_data(img, output_type=pytesseract.Output.DICT, lang=lang)
+
+                file.write(f"图片文件: {image_file}, 语言: {lang}\n")
+                for i in range(len(data["text"])):
+                    text = data["text"][i]
+                    left = data["left"][i]
+                    top = data["top"][i]
+                    width = data["width"][i]
+                    height = data["height"][i]
+                    confidence = data["conf"][i]
+                    if text:
+                        file.write(f"{text} ")
+                        print(
+                            f"\033[32m[INFO]: 识别结果 ({lang}): {text}(坐标: 左={left}, 上={top}, 宽={width}, 高={height}, 置信度={confidence})\033[0m")
+                    # 输出识别结果到控制台，包含语言信息
+                file.write("\n")
+
+    print(f"\033[32m[INFO]: 识别结果和文字坐标已写入文件: {file_path}\033[0m")
+    print("\033[32m[INFO]: 如果txt文档乱码请用UTF-8编码打开文件~\033[0m")
+    root.destroy()
+    input("\033[32m[INFO]: 按回车键退出......\033[0m")
+
+
+def display_menu():
+    print("\033[32m请选择功能:\033[0m")
+    print("\033[32m1. 自动化图像识别\033[0m")
+    print("\033[32m2. 文字识别\033[0m")
+    choice = input("\033[32m请输入选项: \033[0m")
+
+    if choice == "1":
+        perform_image_recognition()
+    elif choice == "2":
+        perform_text_recognition()
+    else:
+        print("\033[32m[INFO]:无效选项，请重新选择\033[0m")
+        display_menu()
 
 
 if __name__ == "__main__":
-    main()
+    display_menu()
